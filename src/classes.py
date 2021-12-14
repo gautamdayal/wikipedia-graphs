eb443e0f5983fd4c81a4d80c7bd1a009ec1eaefe
Gautam Dayal <gautam.dayal@yahoo.com>
2021-12-13 19:17:17 -0600

    improved edge storage method

--- src/classes.py
+++ src/classes.py
@@ -24,17 +24,15 @@ class WikiPage(object):
                 return not any(['wiki' not in link, ':' in link, 'wikipedia' in link, 'Main_Page' in link, 'identifier' in link])
         reqs = requests.get(self.makeUrl())
         soup = BeautifulSoup(reqs.text, 'html.parser')
-        edges = {'src':[], 'dest': []}
+        edges = {self.link:[]}
         for child in soup.find_all('a'):
             if isValid(child.get('href'), child.get('title')):
                 if only_link:
-                    if child.get('href') not in edges['dest']:
-                        edges['src'].append(self.link)
-                        edges['dest'].append(child.get('href'))
+                    if child.get('href') not in edges[self.link]:
+                        edges[self.link].append(child.get('href'))
                 else:
-                    if WikiPage(child.get('href'), child.get('title')) not in edges['dest']:
-                        edges['src'].append(self)
-                        edges['dest'].append(WikiPage(child.get('href'), child.get('title')))
+                    if WikiPage(child.get('href'), child.get('title')) not in edges[self.link]:
+                        edges[self.link].append(child)
         return edges
 
     def make_csv(self):
--- src/create_data.py
+++ src/create_data.py
@@ -1,4 +1,12 @@
 from classes import *
 
 node_0 = WikiPage('/wiki/Kerbal_Space_Program', 'Kerbal Space Program')
-print(node_0.make_csv())
+# print(node_0.make_csv())
+
+# def createSubgraph(root, breadth, depth):
+#     out_file = open(f"../data/{root.title}_{breadth}_{depth}", 'w')
+#     out_dict = {}
+#
+#     out_file.close()
+
+print(node_0.crawl(only_link=True))
